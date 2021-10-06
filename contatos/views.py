cba932df91fa477b67a06e0503d58abe3e2ed6b6
Gustavo Sacchi <gusacchisilva@gmail.com>
2021-10-06 20:23:50 -0300

    Versão 1.4 - Criando a barra superior a partir de um template free e adicionando a funcao de pesquisa.

--- contatos/views.py
+++ contatos/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
     try:
-        contatos = Contato.objects.all().order_by('id')
+        contatos = Contato.objects.order_by('id').filter(
+            mostrar=True
+        )
 
         ### criando a paginação
         paginator = Paginator(contatos, 10)
-        page = request.GET.get('p', 1)
-        #contatos = paginator.get_page(page)
+        page = request.GET.get('p')
+        contatos = paginator.get_page(page)
 
-        try:
-            contatos = paginator.page(page)
-        except PageNotAnInteger:
-            contatos = paginator.page(1)
-        except EmptyPage:
-            contatos = paginator.page(paginator.num_pages)
+        # try:
+        #     contatos = paginator.page(page)
+        # except PageNotAnInteger:
+        #     contatos = paginator.page(1)
+        # except EmptyPage:
+        #     contatos = paginator.page(paginator.num_pages)
 
         return render(request, 'contatos/index.html', {
             'contatos': contatos
@@ -28,10 +32,46 @@ def index(request):
 
 def pg_contato(request, contato_id):
     try:
-        contato = Contato.objects.get(id = contato_id)
-        #contato = get_object_or_404(Contato, id=contato_id)
-        return render(request, 'contatos/pg_contato.html', {
-            'contato': contato
+        contato = Contato.objects.get(id=contato_id)
+        # contato = get_object_or_404(Contato, id=contato_id)
+
+        if not contato.mostrar:
+            return render(request, 'contatos/404.html')
+        else:
+            return render(request, 'contatos/pg_contato.html', {
+                'contato': contato
+            })
+    except Contato.DoesNotExist as e:
+        return render(request, 'contatos/404.html')
+
+
+def busca(request):
+    try:
+        ### pegando valor da pagina
+        termo = request.GET.get('termo')
+
+        ### Evitar busca sem termo (<> termo vazio)
+        if termo is None:
+            return render(request, 'contatos/404.html')
+
+
+        campos = Concat('nome', Value(' '), 'sobrenome')
+
+        contatos = Contato.objects.annotate(
+            nome_completo=campos).filter(
+            nome_completo__icontains=termo
+        )
+
+
+        ### ver a query: print(contatos.query)
+
+        ### criando a paginação
+        paginator = Paginator(contatos, 10)
+        page = request.GET.get('p')
+        contatos = paginator.get_page(page)
+
+        return render(request, 'contatos/busca.html', {
+            'contatos': contatos
         })
     except Contato.DoesNotExist as e:
         return render(request, 'contatos/404.html')
