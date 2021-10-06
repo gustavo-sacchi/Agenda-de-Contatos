c28f518235a805f3a945b9984c957e54e7278c3b
Gustavo Sacchi <gusacchisilva@gmail.com>
2021-10-05 21:52:07 -0300

    Versão 1.2 - Criando models e views

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria', 'categoria_especial')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('categoria_especial', 'categoria')
+    list_per_page = 20
+
+
+admin.site.register(Contato, ContatoAdmin)
+admin.site.register(Categoria)
